f1a9302d5ce1976339fcff5efa7d84af9801463d
josua-stng <josuasitanggang51@gmail.com>
2023-05-12 12:47:13 +0700

    Add: component skilss

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner"
 import Navbar from "./Navbar"
+import Skills from "./Skills"
 
 const Homepage =() =>{
     return(
         <div>
            <Navbar/>
            <Banner/>
+           <Skills/>
         </div>
     )
 }
