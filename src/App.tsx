c0b222ee632912f444948e2d8f1accdbfc029e51
josua-stng <josuasitanggang51@gmail.com>
2023-05-15 20:03:44 +0700

    feat:create routing for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes ,Route} from "react-router-dom";
 import Homepage from "./components/Homepage";
+import AllProject from "./project-page/AllProject";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage/>} />
+        <Route path="/project" element={<AllProject/>}/>
       </Routes>
     </>
   )
