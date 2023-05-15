136200d944539e4d602acfc1c6b5344c50dd5308
josua-stng <josuasitanggang51@gmail.com>
2023-05-15 12:15:27 +0700

    fix:footer mobile design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import { HeartIcon } from "@heroicons/react/24/solid"
 const Footer =() =>{
     return(
-        <div className="text-white text-center mt-32 mb-5 font-serif flex justify-center gap-1">
+        <div className="text-white text-center mt-32 mb-5 font-serif flex justify-center">
             <p>Created with</p>
-          <HeartIcon className="w-5 text-red-400"/>
+          <HeartIcon className="w-5 text-red-400 mr-1 ml-1"/>
             <span className="font-bold">by Josua Sitanggang</span>
         </div>
     )
