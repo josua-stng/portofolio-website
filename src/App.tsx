d5f22421a083a2bc4bce0cf527c5b48bb5279fa8
josua-stng <josuasitanggang51@gmail.com>
2023-05-16 09:25:43 +0700

    Add:routing component about

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes ,Route} from "react-router-dom";
 import Homepage from "./components/Homepage";
 import AllProject from "./project-page/AllProject";
+import AboutMe from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage/>} />
         <Route path="/project" element={<AllProject/>}/>
+        <Route path="/about" element={<AboutMe/>}/>
       </Routes>
     </>
   )
