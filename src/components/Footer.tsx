d722a22fda09ffa1e714d57809404ae422f819e7
josua-stng <josuasitanggang51@gmail.com>
2023-05-16 09:26:13 +0700

    fix:animatiion footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { HeartIcon } from "@heroicons/react/24/solid"
 const Footer =() =>{
     return(
-        <div className="text-white text-center mt-32 mb-5 font-serif flex justify-center">
+        <div className="text-white text-center mt-32 mb-5 font-serif flex justify-center animate__animated animate__fadeInDown">
             <p>Created with</p>
           <HeartIcon className="w-5 text-red-400 mr-1 ml-1"/>
             <span className="font-bold">by Josua Sitanggang</span>
