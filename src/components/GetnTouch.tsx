d42ec1d8de9e48df39186e132228aeabb5122ad4
josua-stng <josuasitanggang51@gmail.com>
2023-05-17 09:56:26 +0700

    fix:link route to contact

--- src/components/GetnTouch.tsx
+++ src/components/GetnTouch.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const GetInTouch = () => {
   return (
     <div className="text-zinc-300 text-center mx-auto border-2 border-gray-200 max-w-md mt-20 bg-stone-800 rounded-xl ">
@@ -9,9 +11,11 @@ const GetInTouch = () => {
           <p>around you</p>
         </div>
 
+    <Link to='/contact'>
         <div className="mt-6 mb-5 border-2 border-gray-200 w-max m-auto px-16 py-3 cursor-pointer rounded-xl hover:bg-gradient-to-l hover:from-red-500 hover:to-yellow-500 transition-all duration-500 ease-in-out slide_right hover:text-black">
           <button>Let's Connect</button>
         </div>
+    </Link>
       </div>
     </div>
   );
